088667f4f928f921eb24b2d434d6830c4afa90cd
Gustavo Pedroso Gal <gustavopgal@gmail.com>
2022-12-02 00:53:44 -0300

    go

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 const {Pool} = require('pg')
 
-//const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
-const connectionString = `postgres://zrozxsdw:z9lZYPYaeqj_PbtNwnh2Tz8A6R0DD13D@babar.db.elephantsql.com/zrozxsdw`
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+//const connectionString = `postgres://zrozxsdw:z9lZYPYaeqj_PbtNwnh2Tz8A6R0DD13D@babar.db.elephantsql.com/zrozxsdw`
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL: connectionString
